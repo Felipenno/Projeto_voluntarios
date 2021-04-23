f1d12b127121acf90468d41695a0c749293795d1
Martin Obrecht <martin.obrecht@yahoo.com.br>
2021-04-23 19:08:41 -0300

    UsuarioController função update ok!

--- WebAPI/src/app/controllers/UsuarioController.js
+++ WebAPI/src/app/controllers/UsuarioController.js
@@ -7,7 +7,18 @@ class UsuarioController {
 		
 	}
 
-	
+	async update(request, response) {
+		const id = request.params.id;
+
+		await Usuario.update(request.body, { where: { id_usuario: id}})
+		.then(usuario => {
+			if (usuario == 1) {
+				response.send({ message: "Usuário atualizado!"});
+			} else {
+				response.send({ messsage: "Não foi possível atualizar o usuário!"});
+			}
+		});
+	}
 }
 
 export default new UsuarioController();
--- WebAPI/src/routes.js
+++ WebAPI/src/routes.js
@@ -3,7 +3,7 @@ import UsuarioController from './app/controllers/UsuarioController';
 
 const routes = new Router();
 
-routes.post('/usuario', UsuarioController.store);
+routes.put('/usuarios/:id', UsuarioController.update);
 
 
 export default routes;
