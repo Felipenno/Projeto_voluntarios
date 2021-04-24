218116f5a5a2747a0504f64e5423cc2cd741f47a
Guilherme Carmo <jose.balseiro@aluno.faculdadeimpacta.com.br>
2021-04-24 13:39:17 -0300

    adicionando o metodo listar enderecos

--- WebAPI/src/app/controllers/EnderecoController.js
+++ WebAPI/src/app/controllers/EnderecoController.js
@@ -6,6 +6,14 @@ class EnderecoController {
 	async store(request, response) {
 		
 	}
+	async listar(request, response) {
+		try{
+		const endereco = await Endereco.findAll();
+		return response.json(endereco);
+	}catch(err){
+		return response.status(400).json({ error: err.message});
+		}
+	}
 
 	
 }
--- WebAPI/src/routes.js
+++ WebAPI/src/routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
+import EnderecoController from './app/controllers/EnderecoController';
 import UsuarioController from './app/controllers/UsuarioController';
 import VoluntarioControllers from './app/controllers/VoluntarioControllers';
 
 const routes = new Router();
 
 routes.post('/usuarios', UsuarioController.store);
-routes.put('/usuarios/:id', UsuarioController.update);
 routes.get('/usuario', UsuarioController.index);
 routes.get('/usuario/:id', UsuarioController.show);
 routes.put('/usuario/:id', UsuarioController.editar);
@@ -14,6 +14,8 @@ routes.delete('/usuario/:id', UsuarioController.destroy);
 routes.get('/voluntario', VoluntarioControllers.index);
 routes.delete('/voluntario/:id', VoluntarioControllers.destroy);
 
+
+routes.get('/enderecos', EnderecoController.listar);
 
 
 
