b3a2a220ce3cc46280f19de861e2547d12e46683
Guilherme Carmo <jose.balseiro@aluno.faculdadeimpacta.com.br>
2021-04-24 01:50:31 -0300

    adicionando o metodo listar em usuario, o metodo puxa o seu endereco junto

--- WebAPI/src/app/controllers/UsuarioController.js
+++ WebAPI/src/app/controllers/UsuarioController.js
@@ -1,10 +1,20 @@
 import Usuario from '../models/Usuario';
+import Endereco from '../models/Endereco';
 
 class UsuarioController {
 
 	
 	async store(request, response) {
 		
+	}
+	async index(request, response){
+
+    const usuarios = await Usuario.findAll({
+      include: { association: 'endereco' }
+    });
+
+    return response.json(usuarios);
+
 	}
 
 	
--- WebAPI/src/routes.js
+++ WebAPI/src/routes.js
@@ -4,6 +4,7 @@ import UsuarioController from './app/controllers/UsuarioController';
 const routes = new Router();
 
 routes.post('/usuario', UsuarioController.store);
+routes.get('/usuarios', UsuarioController.index);
 
 
 export default routes;
