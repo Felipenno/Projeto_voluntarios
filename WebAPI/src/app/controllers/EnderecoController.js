8e33ed19dc49935f2cbe470efc9d265e1f03fbea
Felipe Nunes <felipenno14@gmail.com>
2021-04-24 18:51:34 -0300

    Criado o metodo store em EndereçoController

--- WebAPI/src/app/controllers/EnderecoController.js
+++ WebAPI/src/app/controllers/EnderecoController.js
@@ -1,10 +1,29 @@
 import Endereco from '../models/Endereco';
+import Usuario from '../models/Usuario';
 
 class EnderecoController {
 
-	
 	async store(request, response) {
+		 
+		const { id } = request.params;
+
+		const usuario = await Usuario.findByPk( id )
+
+		if(!usuario){
+			return request.status(400).send({ erro: "Usuário não existe"})
+		}
+
+		const enderecoBody = request.body;
+		enderecoBody.fk_id_usuario = id;
 		
+		await Endereco.create(enderecoBody)
+		.then(data => {
+			return response.send(data);
+		}).catch(erro => {
+			return response.status(500).send({message: `Erro interno: ( ${erro} )`});
+		});
+
+		response.send(endereco);
 	}
 
 	
--- WebAPI/src/routes.js
+++ WebAPI/src/routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import UsuarioController from './app/controllers/UsuarioController';
 import VoluntarioControllers from './app/controllers/VoluntarioControllers';
+import EnderecoController from './app/controllers/EnderecoController';
 
 const routes = new Router();
 
 routes.post('/usuarios', UsuarioController.store);
-routes.put('/usuarios/:id', UsuarioController.update);
 routes.get('/usuario', UsuarioController.index);
 routes.get('/usuario/:id', UsuarioController.show);
 routes.put('/usuario/:id', UsuarioController.editar);
@@ -14,9 +14,7 @@ routes.delete('/usuario/:id', UsuarioController.destroy);
 routes.get('/voluntario', VoluntarioControllers.index);
 routes.delete('/voluntario/:id', VoluntarioControllers.destroy);
 
-
-
-
+routes.post('/usuarios/endereco/:id', EnderecoController.store);
 
 
 export default routes;
