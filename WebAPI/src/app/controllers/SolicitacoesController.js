59dea1a6558f88f57b7d35ab7560418a6ab89b99
Martin Obrecht <martin.obrecht@yahoo.com.br>
2021-04-25 14:59:52 -0300

    Criar Solicitacoes ok

--- WebAPI/src/app/controllers/SolicitacoesController.js
+++ WebAPI/src/app/controllers/SolicitacoesController.js
@@ -1,13 +1,35 @@
 import Solicitacoes from '../models/Solicitacoes';
+import Usuario from '../models/Usuario';
 
 class SolicitacoesController {
-
 	
 	async store(request, response) {
 		
-	}
+		const id_usuario = request.params.id;
 
-	
+		const { id_solicitacoes, servico, dia, status, descrica_problema, data_criacao, data_encerramento, nota} = request.body
+
+		await Solicitacoes.create({
+			servico: servico,
+			dia: dia,
+			status: status,
+			descrica_problema: descrica_problema,
+			data_criacao: data_criacao,
+			data_encerramento: data_encerramento,
+			nota: nota,
+			uSolicitacoes: [
+				{fk_id_usuario: id_usuario},
+				{fk_id_solicitacoes: id_solicitacoes}
+			]
+		}, {
+			include: [ 'usuarios' ]
+		})
+		.then(data => {
+			return response.send(data)
+		}).catch( err => {
+			return response.status(500).send({ message: `Erro interno: ${err}.`});
+		});
+	}	
 }
 
 export default new SolicitacoesController();
--- WebAPI/src/routes.js
+++ WebAPI/src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import SolicitacoesController from './app/controllers/SolicitacoesController';
 import UsuarioController from './app/controllers/UsuarioController';
+
 
 const routes = new Router();
 
 routes.post('/usuarios', UsuarioController.store);
 routes.put('/usuarios/:id', UsuarioController.update);
+
+routes.post('/usuarios/:id/solicitacoes', SolicitacoesController.store);
 
 
 export default routes;
