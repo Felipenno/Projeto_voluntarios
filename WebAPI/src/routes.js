ae07f05b3b2d26227d52de6ef17ce1083620ac52
mariana-leite92 <leite.mari92@gmail.com>
2021-04-25 00:30:55 -0300

    Atualizacao de Solicitacoes

--- WebAPI/src/routes.js
+++ WebAPI/src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import SolicitacoesController from './app/controllers/SolicitacoesController';
 import UsuarioController from './app/controllers/UsuarioController';
 import VoluntarioControllers from './app/controllers/VoluntarioControllers';
 
@@ -9,5 +10,7 @@ routes.post('/usuario', UsuarioController.store);
 routes.post('/voluntario', VoluntarioControllers.store);
 
 routes.put('/voluntario/:id', VoluntarioControllers.update);
+
+routes.put('solicitacoes/:id', SolicitacoesController.index)
 
 export default routes;
