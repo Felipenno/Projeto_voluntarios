bfb223a169c754f700f6f9bbca2ed7c65f9d556b
Guilherme Carmo <jose.balseiro@aluno.faculdadeimpacta.com.br>
2021-05-10 14:27:03 -0300

    adicionando validacao em atualizar endereco

--- Voluntarios-app/src/app/components/endereco-atualizar/endereco-atualizar.component.ts
+++ Voluntarios-app/src/app/components/endereco-atualizar/endereco-atualizar.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Endereco } from 'src/app/models/Endereco';
 import { EnderecoService } from 'src/app/services/endereco.service';
+
 
 @Component({
   selector: 'app-endereco-atualizar',
@@ -12,15 +14,28 @@ import { EnderecoService } from 'src/app/services/endereco.service';
 export class EnderecoAtualizarComponent implements OnInit {
 
   endereco: Endereco = new Endereco();
+  enderecoForm: FormGroup
 
 
 
   constructor(private enderecoService: EnderecoService,
               private router: Router,
-              private toastr: ToastrService) { }
+              private toastr: ToastrService,
+              private formBuilder: FormBuilder) { }
+
+              get ef(): any{
+                return this.enderecoForm.controls; 
+              }
 
   ngOnInit(): void {
     this.preencherEndereco()
+    this.validacao()
+  }
+  validacao():void{
+    this.enderecoForm = this.formBuilder.group({
+      cep: ['', [Validators.required, Validators.maxLength(20)]],
+      estado: ['', [Validators.required, Validators.maxLength(30)]]
+    });
   }
 
   preencherEndereco():void{
@@ -37,6 +52,7 @@ export class EnderecoAtualizarComponent implements OnInit {
   }
 
   atualizarEndereco():void{
+    this.endereco = this.enderecoForm.value;
     this.enderecoService.atualizarEndereco(this.endereco).subscribe({
       next: data => {
         this.toastr.success('Atualizado com sucesso', 'Atualizado');
